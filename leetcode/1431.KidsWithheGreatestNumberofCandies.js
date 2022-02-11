a3144400a57c6d91eb9e4aa250da74947ad4e50a
Jonathon Lee <jonnyleealas@gmail.com>
2022-02-11 12:28:12 -0600

    Create 1431.KidsWithheGreatestNumberofCandies.js

---
+++ leetcode/1431.KidsWithheGreatestNumberofCandies.js
@@ -0,0 +1,46 @@
+```
+There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
+
+Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
+
+Note that multiple kids can have the greatest number of candies.
+
+ 
+
+Example 1:
+
+Input: candies = [2,3,5,1,3], extraCandies = 3
+Output: [true,true,true,false,true] 
+Explanation: If you give all extraCandies to:
+- Kid 1, they will have 2 + 3 = 5 candies, which is the greatest among the kids.
+- Kid 2, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+- Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
+- Kid 4, they will have 1 + 3 = 4 candies, which is not the greatest among the kids.
+- Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+```
+
+
+var kidsWithCandies = function (candies, extraCandies) {
+  let result = []
+  let max = 0
+// save the max number in candies and save it
+  for (let i in candies) {
+    if (candies[i] >= max) {
+      max = candies[i]
+    }
+  }
+// loop through candies and add candies[i] + extra candies
+
+  for (let i in candies) {
+    // if its less than max push false
+    if (candies[i] + extraCandies < max) {
+      result.push(false);
+    } else {
+      // if it isnt less than push true
+      result.push(true);
+    }
+  }
+  // return result
+  
+  return result
+};
